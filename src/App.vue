ad30523acb228f6dcd2d7ad7efe98dc8080dd085
RubenK00 <ruben2412000@gmail.com>
2020-05-09 15:44:46 +0200

    Added ToDo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <todo />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import todo from './components/todo.vue'
 
-export default {
+  export default {
   name: 'App',
   components: {
-    HelloWorld
+    todo
   }
 }
 </script>
---
+++ src/components/todo.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h1>ToDos</h1>
+        <input v-model="newItem" type="text" /><button v-on:click="addItem">Add</button>
+        <div>
+            <p v-for="(e, index) in myToDos" v-on:click="itemDone(index)" v-bind:key="e">{{ e }}</p>
+        </div>
+        <div class="listDone">
+            <p v-for="(e, index) in myDoneToDos" v-on:click="deleteItem(index)" v-bind:key="e">{{ e }}</p>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: "todo",
+        data(){
+            return{
+                myToDos: [],
+                myDoneToDos: [],
+                newItem: ""
+            }
+        },
+        methods:{
+            addItem: function () {
+                this.myToDos.push(this.newItem)
+                this.newItem = ""
+            },
+            itemDone: function (index) {
+                this.myDoneToDos.push(this.myToDos.splice(index, 1)[0])
+            },
+            deleteItem: function (index) {
+                this.myDoneToDos.splice(index,1)
+            }
+
+        }
+
+    }
+</script>
+<style>
+    .listDone{
+        text-decoration: line-through;
+    }
+</style>
