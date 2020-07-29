7ff003e80a8b6ccd7b5e4481f0e05bc52ad45e42
Ruben Kaiser <rkaiser@adobe.com>
2020-07-29 14:32:49 +0200

    expressJS Workshop preparation

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -11,6 +11,7 @@
     </div>
 </template>
 <script>
+    import axios from 'axios'
     export default {
         name: "todo",
         data(){
@@ -24,15 +25,48 @@
             addItem: function () {
                 this.myToDos.push(this.newItem)
                 this.newItem = ""
+                this.saveChanges()
             },
             itemDone: function (index) {
                 this.myDoneToDos.push(this.myToDos.splice(index, 1)[0])
+                this.saveChanges()
             },
             deleteItem: function (index) {
                 this.myDoneToDos.splice(index,1)
+                this.saveChanges()
+            },
+
+            //Not Part of the simple Vue Workshop, for expressJS Workshop
+            saveChanges: function () {
+                const me = this;
+                axios.post('http://localhost:3000/', {
+                    myToDos: me.myToDos,
+                    myDoneToDos: me.myDoneToDos
+                })
+                    .then(function (response) {
+                        console.log(response.data);
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            },
+            init: function () {
+                const me = this
+                axios.get('http://localhost:3000/')
+                    .then(function (response) {
+                        me.myDoneToDos = response.data.myDoneToDos
+                        me.myToDos = response.data.myToDos
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
             }
 
+        },
+        mounted() {
+            this.init()
         }
+        // End of expressJS Workshop part
 
     }
 </script>
